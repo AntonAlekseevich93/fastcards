65f95e3efb84a7d7d0e25b4b3c5b837e86796cb6
AntonAlekseevich93 <a.bel93@mail.ru>
2023-09-21 13:49:52 +0300

    - updated config

--- src/main/kotlin/com/fastcards/Application.kt
+++ src/main/kotlin/com/fastcards/Application.kt
@@ -11,16 +11,15 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.jetbrains.exposed.sql.Database
 
-fun main() {
-    embeddedServer(Netty, port = 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hello, MAN!")
-            }
-        }
-    }.start(wait = true)
-}
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
+        }
+    }
+}
 //fun main() {
 //
 ////    Database.connect(
@@ -39,6 +38,3 @@ fun main() {
 //        .start(wait = true)
 //}
 
-fun Application.module() {
-
-}
