588481338b7131a1a7e2aad437988a0e5a7f1eba
AntonAlekseevich93 <a.bel93@mail.ru>
2023-09-21 14:35:59 +0300

    - updated config

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,9 +49,9 @@ repositories {
     maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
 }
 
-//tasks {
-//    create("stage").dependsOn("installDist")
-//}
+tasks {
+    create("stage").dependsOn("installDist")
+}
 
 
 
